566e2b77d43a0b00623bb57343f58da294cc83af
Chris Gardner <ctgardner89@gmail.com>
2016-05-27 21:05:07 -0400

    fixed that typo and adjusted accordinly

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
 
-    var $bhold = $("#bholder");
-    setTimeout(function(){
-      $bhold.scrollBy(0,1);
-    },2)
+    
    //this.css("text-shadow","3px 5px 10px gold");
     //$("nav").css("text-shadow","3px 5px 10px blue").toggle();
 
