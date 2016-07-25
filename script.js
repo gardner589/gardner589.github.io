f4e34696d626b603bbeb14a22f8d54a8395e68a7
Chris Gardner <ctgardner89@gmail.com>
2016-07-24 23:00:06 -0400

    tweak session

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
 
+  $('#face h1').hide();
+  $('#face p').hide();
+  $('#face h1').toggle(1050);
+  $('#face p').toggle(1050);
+  // $('#face img').hover(function(){
+  //   $(this).parent().find($('h1')).toggle(1500);
+  //   $(this).parent().find($('p')).toggle(1500);
+  // })
+  $('#face img').click(function(){
+    $('#face h1').toggle(1000);
+    $('#face p').toggle(1000);
+  })
+
 
    //this.css("text-shadow","3px 5px 10px gold");
     //$("nav").css("text-shadow","3px 5px 10px blue").toggle();
