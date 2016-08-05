d5035553a72ad5d087d14d17a9e903f889459392
Chris Gardner <ctgardner89@gmail.com>
2016-08-05 15:18:06 -0400

    this is a commit

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
 
+  $('#content').toggle()
   $('#face h1').hide();
   $('#face p').hide();
   $('#face h1').toggle(1050);
   $('#face p').toggle(1050);
+  $('#about').click(function(){
+    $('#content').toggle(1500)
+  })
   // $('#face img').hover(function(){
   //   $(this).parent().find($('h1')).toggle(1500);
   //   $(this).parent().find($('p')).toggle(1500);
@@ -12,6 +16,7 @@ $(document).ready(function(){
   //   $('#face h1').toggle(1000);
   //   $('#face p').toggle(1000);
   // })
+
 
 
    //this.css("text-shadow","3px 5px 10px gold");
