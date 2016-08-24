6d363843750a4002e8070db2cefe68b1c851201a
Chris Gardner <ctgardner89@gmail.com>
2016-08-24 17:22:15 -0400

    some bug fixings

--- script.js
+++ script.js
@@ -5,17 +5,26 @@ $(document).ready(function(){
   $('#skills').toggle()
   $('.about').click(function(){
     $('#about-show').toggle(1000)
+    $('html, body').animate({
+      scrollTop: $("#about-show").offset().top
+    }, 2000);
   })
   $('.projects').click(function(){
     $('#projects').toggle(1000)
+    $('html, body').animate({
+      scrollTop: $("#projects").offset().top
+    }, 2000);
   })
   $('.skills').click(function(){
     $('#skills').toggle(1000)
+    $('html, body').animate({
+      scrollTop: $("#skills").offset().top
+    }, 2000);
     x=0;
     addr = setInterval(function(){
         $('#skills').append(skillsBlob[x]+', ')
         x++
-        if(x == skillsBlob.length){clearInterval(addr)}
+        if(x == skillsBlob.length-1){clearInterval(addr)}
     },1500)
   })
 })
