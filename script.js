6bac1a98fa6bf4145a49e683762a8b042e9a9409
Chris Gardner <ctgardner89@gmail.com>
2016-08-31 16:04:59 -0400

    updates

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var skillsBlob = ['Javascript','jQuery','Ruby on Rails','AngularJS','React.js','Node.js','Express.js','PostgresSQL','AJAX','JSON','Git','Github','HTML5','CSS3','MongoDB','Bootstrap','Activerecord','mongoose ODM']
+  var skillsBlob = ['Javascript','jQuery','Ruby on Rails','AngularJS','React.js','Node.js','Express.js','PostgresSQL','AJAX','JSON','Git','Github','HTML5','CSS3','MongoDB','Bootstrap','Activerecord','mongoose ODM', 'Jasmine', 'Rspec', 'Selenium']
   $('#about-show').toggle()
   $('#projects').toggle()
   $('#skills').toggle()
@@ -16,15 +16,18 @@ $(document).ready(function(){
     }, 2000);
   })
   $('.skills').click(function(){
+    $('#skills p').html('')
     $('#skills').toggle(1000)
     $('html, body').animate({
       scrollTop: $("#skills").offset().top
     }, 2000);
     x=0;
     addr = setInterval(function(){
-        $('#skills').append(skillsBlob[x]+', ')
+        $('#skills p').append(' * '+skillsBlob[x])
         x++
-        if(x == skillsBlob.length-1){clearInterval(addr)}
-    },1500)
+        if(x == skillsBlob.length){
+          clearInterval(addr)
+        }
+    },150)
   })
 })
