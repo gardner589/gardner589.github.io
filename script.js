04ea105f60b241d36bd3dc8ce2e7e74fc1797a7b
Chris Gardner <ctgardner89@gmail.com>
2016-09-12 09:41:04 -0400

    some more style tweaks

--- script.js
+++ script.js
@@ -7,27 +7,27 @@ $(document).ready(function(){
     })
 
   var skillsBlob = ['Javascript','jQuery','Ruby on Rails','AngularJS','React.js','Node.js','Express.js','PostgresSQL','AJAX','JSON','Git','Github','HTML5','CSS3','MongoDB','Bootstrap','Activerecord','mongoose ODM', 'Jasmine', 'Rspec', 'Selenium']
-  $('#about-show').toggle()
-  $('#projects').toggle()
-  $('#skills').toggle()
-  $('.about').click(function(){
-    $('#about-show').toggle(1000)
-    $('html, body').animate({
-      scrollTop: $("#about-show").offset().top
-    }, 2000);
-  })
-  $('.projects').click(function(){
-    $('#projects').toggle(1000)
-    $('html, body').animate({
-      scrollTop: $("#projects").offset().top
-    }, 2000);
-  })
-  $('.skills').click(function(){
-    $('#skills p').html('')
-    $('#skills').toggle(1000)
-    $('html, body').animate({
-      scrollTop: $("#skills").offset().top
-    }, 2000);
+  // $('#about-show').toggle()
+  // $('#projects').toggle()
+  // $('#skills').toggle()
+  // $('.about').click(function(){
+  //   $('#about-show').toggle(1000)
+  //   $('html, body').animate({
+  //     scrollTop: $("#about-show").offset().top
+  //   }, 2000);
+  // })
+  // $('.projects').click(function(){
+  //   $('#projects').toggle(1000)
+  //   $('html, body').animate({
+  //     scrollTop: $("#projects").offset().top
+  //   }, 2000);
+  // })
+  // $('.skills').click(function(){
+  //   $('#skills p').html('')
+  //   $('#skills').toggle(1000)
+  //   $('html, body').animate({
+  //     scrollTop: $("#skills").offset().top
+  //   }, 2000);
     x=0;
     addr = setInterval(function(){
         $('#skills p').append(' * '+skillsBlob[x])
@@ -36,5 +36,5 @@ $(document).ready(function(){
           clearInterval(addr)
         }
     },150)
-  })
+  // })
 })
