a50dbcb4171dc5333e9db8b5c84d609a8bafe62f
Chris Gardner <ctgardner89@gmail.com>
2016-09-21 17:29:47 -0400

    tweaks bro... tweaks

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ $(document).ready(function(){
     $('#face').css('letter-spacing', $(this).scrollTop()/30+'px')
     $('#myFace').css('transform','rotate('+$(this).scrollTop()+'deg)')
     $('.contact img').css('transform','rotate('+(-$(this).scrollTop())+'deg)')
+    // var faceScroll = $('#face').scrollTop()
+    $('#face').css('background-position-y', $(this).scrollTop()/-5+'px')
   })
   $('#face').mousemove(function(evt){
     $('.navg8 div').css({
@@ -16,6 +18,10 @@ $(document).ready(function(){
     "margin-right": evt.clientX/30+"px",
     "margin-top": evt.clientY/-30+"px",
     "margin-bottom": evt.clientY/30+"px"
+    })
+    $('#face').css({
+      'background-position-y': evt.clientY/-50+"px",
+      'background-position-x': evt.clientX/-50+"px"
     })
   })
   var skillsBlob = ['Javascript','jQuery','Ruby on Rails','AngularJS','React.js','Node.js','Express.js','PostgresSQL','AJAX','JSON','Git','Github','HTML5','CSS3','MongoDB','Bootstrap','Activerecord','mongoose ODM', 'Jasmine', 'Rspec', 'Selenium']
